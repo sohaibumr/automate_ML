20a421fd3d61edbd9e69c427abbdbc4b1b872517
Sohaib Umer <80094248+sohaibunist@users.noreply.github.com>
2023-03-09 01:13:35 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+
+min_requirements = [
+    'numpy<=1.23.5',
+    'scikit-learn',
+    'pandas',
+    'matplotlib',
+    'scikit-optimize',
+    'lightgbm',
+    'catboost',
+    'seaborn',
+    'scipy',
+    'scikit-optimize',
+    'tabulate',
+    'scikit-plot'
+    ]
+
+
+
+setup(
+
+    name='automate_ML',
+    version="1.0.5",
+
+    author='Sohaib Umer',
+    author_email='sohaibfccu@gmail.com',
+
+    description='A python module to solve machine learning problems in a mechanized way. The repository is able to preprocess the data and output the results in numerical as well as in the graphical form.',
+    long_description='https://github.com/sohaibunist/automate_ML',
+    long_description_content_type="text/markdown",
+
+
+    packages=find_packages(),
+    install_requires=min_requirements,
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+
+        'Programming Language :: Python :: 3'
+
+    ],
+
+)
