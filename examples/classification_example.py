333b79ad6265df763944f9f23ef0fe741faafd8c
Sohaib Umer <80094248+sohaibunist@users.noreply.github.com>
2023-03-09 18:43:38 +0900

    Update classification_example.py

--- examples/classification_example.py
+++ examples/classification_example.py
@@ -8,8 +8,6 @@ X = ['Age', 'Sex', 'BP', 'Cholesterol', 'Na_to_K']
 
 y = 'Drug'
 
-print(data)
-
 clf = Classification(data=data, inputs=X, target=y, problem='Classification', random_state=300)
 clf.Model(model_name='ExtraTrees', random_state=300)
 clf.fit(optimization='Bayesian', num_iter=20, scoring='accuracy')
