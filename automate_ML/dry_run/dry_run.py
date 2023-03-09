547b689ba5667951adbb09432a2a2f3d33b4dd2a
Sohaib Umer <80094248+sohaibunist@users.noreply.github.com>
2023-03-09 17:41:14 +0900

    Update dry_run.py

--- automate_ML/dry_run/dry_run.py
+++ automate_ML/dry_run/dry_run.py
@@ -176,18 +176,3 @@ class all_Models(Preprocessing):
             pass
         if save_txt:
             result.to_csv(filename+'.csv', index_label='Model', header=['Score'])
-
-        """
-        Parameters:
-
-        n_splits 	 = int 		: 	No of splits  (default=100)		 
-        scoring 	 = str 		: 	Scoring method  (default=None)
-        save_txt 	 = bool 	:	Save a txt files with model names and corresponding scores   (default=True)
-        filename 	 = str 		: 	Name of the txt file   (default='Models_score')
-        show 		 = bool  	: 	Print out the sorted table and plots a bar chart of the models with corresponding 
-                                    scores if set True   (default=True)
-        save_fig	 = bool  	: 	Whether to save the figure or not (default=False)
-        fig_name	 = str  	: 	Name of the png file (default=Models Comparison)
-        dpi     	 = int  	: 	Determines the quality of the figure (default=600)
-        
-        """
